a198019162cf683413a296bf72f11b77c285a3da
Marlon Andre Vaez Aguirre <marlonvaez@mpms.mp.br>
2025-04-09 14:54:16 -0400

    Alterando Background box 3x

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -1,21 +1,25 @@
 import Box from "./Box";
 
-<Box
-  tag="div"
-  styleSheet={{
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    backgroundImage: 'url("/pubgCapa.avif")',
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    opacity: 0.2,
-    zIndex: -1,
-  }}
->
-  {/* children entre as tags */}
-  <></>
-</Box>
+const Background = () => (
+  <Box
+    tag="div"
+    styleSheet={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      backgroundImage: 'url("/pubgCapa.avif")',
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      opacity: 0.2,
+      zIndex: -1,
+    }}
+  >
+    {/* children entre as tags */}
+    <></>
+  </Box>
+);
+
+export default Background;
