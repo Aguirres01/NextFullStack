de07bdd62b589d30550fcff35acd89bdc256c59a
Marlon Andre Vaez Aguirre <marlonvaez@mpms.mp.br>
2025-04-09 14:52:17 -0400

    Alterando Background box 2x

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -1,21 +1,21 @@
-import Box from "../components/Box";
+import Box from "./Box";
 
-export default function Background() {
-  return (
-    <Box
-      tag="div"
-      styleSheet={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        backgroundImage: 'url("/pubgCapa.avif")',
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        opacity: 0.2,
-        zIndex: -1,
-      }} children={undefined}    />
-  );
-}
+<Box
+  tag="div"
+  styleSheet={{
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundImage: 'url("/pubgCapa.avif")',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    opacity: 0.2,
+    zIndex: -1,
+  }}
+>
+  {/* children entre as tags */}
+  <></>
+</Box>
